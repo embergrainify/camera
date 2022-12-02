98fc2a6d6fef14dda5a2bbc70ef4bcef7aa0fb65
embergrainify <embergrainify@proton.me>
2022-12-02 16:48:44 +0700

    init

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      <h1 className="text-3xl font-bold">
+        Camera
+      </h1>
+
+
+
+    </div>
+  )
+}
